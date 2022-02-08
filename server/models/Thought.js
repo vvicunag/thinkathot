6aabf3e9d32d03287705a8d98db96f1720b19719
vvicunag <vvicuna1@uc.cl>
2022-02-08 08:34:18 +0100

    add thought model file

--- server/models/Thought.js
+++ server/models/Thought.js
@@ -1,34 +1,29 @@
 const { Schema, model } = require('mongoose');
-const { convertToLowercase, validateEmail } = require("../utils");
 
-const userSchema = new Schema(
+const thoughtSchema = new Schema(
     {
-        username: {
+        thoughtText: {
             type: String, 
             required: true, 
-            unique: true, 
-            trim: true, 
+            minlength: 1,
+            maxlength: 280 
         },
-        email: {
-            type: String,
-            unique: true,
-            validate: [validateEmail, "This is not a valid email address!"]
+        createdAt: {
+            type: Date,
+            //default: //current timestamp,
+            //getter method to format timestamp on query
         },
-        thoughts: [
+        username: [
             {
                 type: Schema.Types.ObjectId,
                 ref: "thought",
             }
         ],
-        friends: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: "user",
-            }
-        ],
+        reactions: [reactionSchema],
     },
     {
         toJSON: {
+            // Create a virtual called `reactionCount` that retrieves the length of the thought's `reactions` array field on query.
             virtuals: true,
           },
           id: false,
@@ -62,18 +57,3 @@ const seed = async () => {
 seed();
 
 module.exports = User;
-
-
-// TODO: Create a virtual called `friendCount` that retrieves the length of the user's `friends` array field on query.
-// userSchema
-//   .virtual('fullName')
-//   // Getter
-//   .get(function () {
-//     return `${this.first} ${this.last}`;
-//   })
-//   // Setter to set the first and last name
-//   .set(function (v) {
-//     const first = v.split(' ')[0];
-//     const last = v.split(' ')[1];
-//     this.set({ first, last });
-//   });
